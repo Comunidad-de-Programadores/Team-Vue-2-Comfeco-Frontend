4de962ccfd40c7c80ddbbb933a218851141c6f35
Gianpierre Galvez Wizeline <gianpierre.galvez@wizeline.com>
2021-02-12 17:21:49 -0500

    Extra configuration of purge on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ["./public/**/*.html", "./src/**/*.vue"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
