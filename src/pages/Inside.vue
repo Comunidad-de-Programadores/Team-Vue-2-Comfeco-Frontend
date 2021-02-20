c57b699e657aa15041564913683fa5e3e989bbb2
Gianpierre Galvez Wizeline <gianpierre.galvez@wizeline.com>
2021-02-20 17:43:48 -0500

    Counter working

--- src/pages/Inside.vue
+++ src/pages/Inside.vue
@@ -1,12 +1,22 @@
 <template lang="pug">
     section
         p inside
+        Counter(:end="end")
 </template>
 <script>
+import Counter from "@/components/Counter/Counter";
 export default {
-    name: 'Inside'
-}
+    name: "Inside",
+    components: {
+        Counter
+    },
+    data() {
+        return {
+            end: new Date(
+                new Date().setMonth(new Date().getMonth() + 12)
+            ).toString()
+        };
+    }
+};
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
