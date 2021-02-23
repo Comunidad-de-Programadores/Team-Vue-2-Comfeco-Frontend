e3b04fd931f21079e26dda06ad691a893a28c52f
Luis Padron <luis57711994@gmail.com>
2021-02-23 19:02:15 -0300

    join button right user in mobile and desktop.

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,21 +1,17 @@
 <template lang="pug">
     div(class="flex flex-col container mx-auto md:flex-row md:items-center md:justify-between mt py-4 px-4")
         TextHeader(:user_connected.sync="user")
-        div(class="md:px-6 py-4 px-2 md:hidden")
-            MenuMovil(:user_connected.sync="user")
         ButtonNavbar(:user_connected.sync="user")            
 </template>
 <script>
 import manageStorage from "../services/manageStorage";
 import ButtonNavbar from "@/components/Header/ButtonNavbar";
-import MenuMovil from "@/components/Header/MenuMovil";
 import TextHeader from "@/components/Header/TextHeader";
 
 export default {
     name: "Header",
     components: {
         ButtonNavbar,
-        MenuMovil,
         TextHeader
     },
     data() {
