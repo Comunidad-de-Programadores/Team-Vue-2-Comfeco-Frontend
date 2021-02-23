4d0ced9d7b5e32901b00483fc50bd428c6e370a1
Luis Padron <luis57711994@gmail.com>
2021-02-22 21:58:10 -0300

    segunda parte

--- src/pages/Inside.vue
+++ src/pages/Inside.vue
@@ -1,10 +1,21 @@
 <template lang="pug">
-    section
-        p inside
+    div
+        div(class="w-full lg:w-3/12")
+            p hola
+        div(class="-mx-8 w-6/12 hidden lg:block")
+            p chao
+        div(class="-mx-8 w-3/12 hidden lg:block")
+            RightSideTalleres
+                                
 </template>
 <script>
+import RightSideTalleres from '@/components/Home/RightSideTalleres'
+
 export default {
-    name: 'Inside'
+    name: 'Inside',
+    components: {
+        RightSideTalleres
+    }
 }
 </script>
 <style lang="scss" scoped>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,7 +4,7 @@
 			div()   
 				div(class="grid md:grid-cols-2 gap-2 mt-7")
 					.text-center(class=" lg:border-gray-300 lg:border-solid lg:border-r-2")
-						router-link( :to=" $route.path !== '/' ? '/' : '' " class=" text-gray-300 dark:text-white text-words font-extrabold tracking-tight w-full rounded-sm" ) Iniciar Sesion
+						router-link( :to=" $route.path !== '/' ? '/' : '' " class=" text-gray-300 text-words dark:text-white font-extrabold tracking-tight w-full rounded-sm" ) Iniciar Sesion
 					.text-center
 						router-link( :to=" $route.path !== 'register' ? 'register' : ''" class="text-gray-300 text-words dark:text-white font-extrabold tracking-tight w-full rounded-sm" ) Registrate
 				LoginForm
