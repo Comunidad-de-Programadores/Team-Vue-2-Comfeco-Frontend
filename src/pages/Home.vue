605ad7031073438b940535f95ea40e8bf1a68a4e
Luis Padron <luis57711994@gmail.com>
2021-02-28 16:14:05 -0300

    stilos

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
-    div(class="grid lg:grid-cols-4 lg:gap-4 grid-cols-1 gap-10 px-4")
-        div(class="col-span-1 hidden lg:block")
-            Communities
-        div(class="w-full lg:col-span-2 ")
-            Counter(:end="end")
-            AutoScrollSlider
-            ManualSlider
-        div(class="col-span-1 hidden lg:block")
-            RightSideTalleres
+    div(class=" z-10 max-w-screen-lg xl:max-w-screen-xl mx-auto")
+        div(class="grid lg:grid-cols-4 lg:gap-4 grid-cols-1 gap-10 px-4 py-4")
+            div(class="col-span-1 hidden lg:block")
+                Communities
+            div(class="w-full lg:col-span-2 ")
+                Counter(:end="end")
+                AutoScrollSlider
+                ManualSlider
+            div(class="col-span-1 hidden lg:block")
+                RightSideTalleres
 </template>
 <script>
 import Communities from "@/components/Communities/Communities";
