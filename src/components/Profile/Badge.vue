2302971ff2df329a3ca68ceb8f6afffd55b9d630
Luis Padron <luis57711994@gmail.com>
2021-03-14 17:19:20 -0300

    badge una sola peticion :P

--- src/components/Profile/Badge.vue
+++ src/components/Profile/Badge.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
     .mt-10   
         div(class="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4")            
-            ul(v-for="(badge, $index) in badges" :key="$index" class="mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative rounded-t-md rounded-b-md")
+            ul(v-for="(badge, $index) in list" :key="$index" class="mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative rounded-t-md rounded-b-md")
                 img(class="w-full rounded-t-md" :src="badge.image_url" alt="")
-                div(v-if="badge.have" class="badge absolute top-0 right-0 bg-purple-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded") Obtenida
+                div(v-if="badge.have_badge" class="badge absolute top-0 right-0 bg-purple-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded") Obtenida
                 div(class="desc p-4 text-gray-800")
                     span(class="title font-bold block") {{ badge.name }}
                     span( class="description text-sm block py-2 border-gray-400 mb-2") {{ badge.description }}
@@ -26,27 +26,15 @@ export default {
         }
     },
     computed:{
-        badges(){
-            if( this.badgesUser && this.list.length > 0 ){
-                return this.list.map( badge => {
-                    this.badgesUser.some(userBadge => userBadge.id === badge.id) ? badge.have = true : badge.have = false
-                    return badge
-                })
-            } else {
-                return []
-            }       
-        }
-    },
-    async created(){
-        this.badgesUser = (await this.badgeService.getBadgesUser()).records
+        
     },
     methods: {
         async infiniteHandler($state) {
-            let response = await this.badgeService.getAllBadges(this.page)   
-
-            if (response.badges.data.length) {
+            let response = await this.badgeService.getBadgesUser(this.page)  
+            
+            if (response.records.data.length) {
                 this.page += 1;
-                this.list.push(...response.badges.data);
+                this.list.push(...response.records.data);
                 $state.loaded();
             } else {
                 $state.complete();
--- src/services/badgeService.js
+++ src/services/badgeService.js
@@ -1,26 +1,12 @@
 import axiosService from "./axiosService";
 
 export default class badgeService {
-    async getBadgesUser(){
-        const url = `v1/users/badges`;
+    async getBadgesUser(page){
+        const url = `v1/users/badges?page=${page}`;
         try {
             const { data } = await axiosService({
                 requiresAuth: true,
                 isGeneralApi: false
-            }).get(url);
-
-            return data;
-        } catch (error) {
-            console.log(error.messages);
-            return Promise.reject(error.messages);
-        }
-    }
-    async getAllBadges(page){
-        const url = `v1/badges?page=${page}`;
-        try {
-            const { data } = await axiosService({
-                requiresAuth: true,
-                isGeneralApi: true
             }).get(url);
 
             return data;
