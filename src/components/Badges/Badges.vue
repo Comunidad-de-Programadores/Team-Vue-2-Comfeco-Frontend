a266249c499ed9e6530cb40dbbd928b377706045
Gianpierre Galvez Wizeline <gianpierre.galvez@wizeline.com>
2021-03-19 08:40:20 -0500

    Badges on profile tab ready

--- src/components/Badges/Badges.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-    h1 Badges
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
--- src/components/Profile/CardBadges.vue
+++ src/components/Profile/CardBadges.vue
@@ -1,33 +1,10 @@
 <template lang="pug">
 section.bg-grey-lightest.py-8
-  .w-full.max-w-2xl.ml-auto.mr-auto.mt-8.bg-white.rounded-md
-    .flex.flex-wrap.justify-center
-      .w-full.px-6.py-6.text-center(class='lg:w-1/4')
-        span.rounded-full.h-24.w-24.font-semibold.inline-block.py-2.px-2.uppercase.text-pink-600.bg-pink-200.uppercase.mr-1(class='last:mr-0')
-          img.rounded-full(v-if="badges[0]" :src="badges[0]", alt="alt")
-          template(v-else)
-            span.rounded-full.font-semibold.pt-9.w-24.h-24.inline-block.py-1.px-2.uppercase.rounded.text-pink-600.bg-pink-200.uppercase.mr-1(class='last:mr-0')
-              | Social
-      .w-full.px-6.py-6.text-center(class='lg:w-1/4')
-        span.rounded-full.font-semibold.w-24.h-24.inline-block.py-1.px-2.uppercase.rounded.text-pink-600.bg-pink-200.uppercase.mr-1(class='last:mr-0')
-          img.rounded-full(v-if="badges[1]" :src="badges[1]", alt="alt")
-          template(v-else)
-            span.rounded-full.font-semibold.pt-9.w-24.h-24.inline-block.py-1.px-2.uppercase.rounded.text-pink-600.bg-pink-200.uppercase.mr-1(class='last:mr-0')
-              | Social
-      .w-full.px-6.py-6.text-center(class='lg:w-1/4')
-        span.rounded-full.font-semibold.pt-9.w-24.h-24.inline-block.py-1.px-2.uppercase.rounded.text-pink-600.bg-pink-200.uppercase.mr-1(class='last:mr-0')
-          template(v-if="badges.length > 2")
-            img.rounded-full( :src="badges[2]", alt="alt")
-          template(v-else)
-            span
-              | Social
-      .w-full.px-6.py-6.text-center(class='lg:w-1/4')
-        span.rounded-full.font-semibold.inline-block.pt-9.w-24.h-24.py-1.px-2.uppercase.rounded.text-pink-600.bg-pink-200.uppercase.mr-1(class='last:mr-0')
-          template(v-if="badges.length > 3")
-            img.rounded-full( :src="badges[3]", alt="alt")
-          template(v-else)
-            span
-              | Social
+    .w-full.max-w-2xl.ml-auto.mr-auto.mt-8.bg-white.rounded-md
+        .flex.flex-wrap.justify-center
+            .w-full.px-6.py-6.text-center(class='lg:w-1/4' v-for='(item,index) in badges' :key="index")
+                span.rounded-full.h-24.w-24.font-semibold.inline-block.py-2.px-2.uppercase.text-pink-600.bg-pink-200.uppercase.mr-1(class='last:mr-0 flex justify-center items-center')
+                    img.rounded-full(:src="item.image_url" :alt="item.name" :class="{'hasNoBadge': item.have_badge === 0}")
 </template>
 
 <script>
@@ -46,16 +23,15 @@ export default {
     methods: {
         async getList() {
             const data = await this.badgeService.getBadgesUser();
-            
-            return data.records.reduce((accum, badge) => {
-              badge.have_badge && accum.push(badge.image_url);
-              return accum
-            }, []);
+            return data.records;
         }
     }
-}
+};
 </script>
 
-<style>
-
+<style lang="scss">
+.hasNoBadge {
+    -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+    filter: grayscale(100%);
+}
 </style>
